059ce6f67242d47542aeb79e47c2af6a611094d9
semaadibelli <semanur.adibelli@gmail.com>
2022-04-08 11:30:40 +0300

    Add project files.

---
+++ Mvc101/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mvc101.Models;
+using Mvc101.Services.SmsService;
+using System.Diagnostics;
+using Mvc101.Services.EmailService;
+
+namespace Mvc101.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ISmsService _smsService;
+        private readonly IEmailService _emailService;
+        private readonly IWebHostEnvironment _appEnvironment;
+
+        public HomeController(ISmsService smsService, IEmailService emailService)
+        {
+            _smsService = smsService;
+            _emailService = emailService;
+        }
+
+        public IActionResult Index()
+        {
+            var result = _smsService.Send(new SmsModel()
+            {
+                TelefonNo = "12345",
+                Mesaj = "home/index çalıştı"
+            });
+
+            var wissenSms = (WissenSmsService)_smsService;
+            Debug.WriteLine(wissenSms.EndPoint);
+
+            var fileStream = new FileStream($"{_appEnvironment.WebRootPath}\\files\\portre.jpeg", FileMode.Open);
+
+            _emailService.SendMailAsync(new MailModel()
+            {
+                To = new List<EmailModel>()
+                {
+                    new EmailModel()
+                    {
+                        Name = "Wissen",
+                        Adress = "site@wissenakademie.com"
+                    }
+                },
+                Subject = "Index Açıldı",
+                Body = "Bu emailin body kısmıdır",
+                Attachs= new List<Stream>()
+                {
+                    fileStream
+                }
+            });
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            var result = _smsService.Send(new SmsModel()
+            {
+                TelefonNo = "12345",
+                Mesaj = "home/index çalıştı"
+            });
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
