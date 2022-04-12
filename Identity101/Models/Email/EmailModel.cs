62fcfe3d950c8bf8d6c0299f28d843c33c74b554
semaadibelli <semanur.adibelli@gmail.com>
2022-04-12 12:05:49 +0300

    confirm email

---
+++ Identity101/Models/Email/EmailModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Identity101.Models.Email
+{ 
+        public class EmailModel
+        {
+            public string Address { get; set; }
+            public string Name { get; set; }
+        }
+
+}
---
+++ Identity101/Services/Email/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using Identity101.Models.Email;
+
+namespace Identity101.Services.Email
+{
+    public interface IEmailService
+    {
+        Task SendMailAsync(MailModel model);
+    }
+}
