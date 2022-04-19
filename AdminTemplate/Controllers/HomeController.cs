7b2b76ea43927e747ec734b8cbdaf426359dd0aa
semaadibelli <semanur.adibelli@gmail.com>
2022-04-19 11:12:18 +0300

    template

---
+++ AdminTemplate/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AdminTemplate.Controllers;
+
+public class HomeController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
