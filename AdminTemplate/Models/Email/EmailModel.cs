a61dfbeb189f50f066c04076bff5e6f86929eb24
semaadibelli <semanur.adibelli@gmail.com>
2022-04-19 15:04:57 +0300

    .

---
+++ AdminTemplate/Models/Email/EmailModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AdminTemplate.Models.Email
+{ 
+        public class EmailModel
+        {
+            public string Address { get; set; }
+            public string Name { get; set; }
+        }
+
+}
