c9dd9d7d846580d03f2963b75b7fdcf37f8eb191
semaadibelli <semanur.adibelli@gmail.com>
2022-04-27 12:03:20 +0300

    color

---
+++ FrontEnd/JavaScript/3.js
@@ -0,0 +1,18 @@
+console.log("3.js");
+var changeColor=() => {
+    var rangeRed=document.getElementById("range-red");
+    var rangeGreen=document.getElementById("range-green");
+    var rangeBlue=document.getElementById("range-blue");
+
+    var pickerDiv=document.getElementById("picker-div");
+
+    console.log([rangeRed.value, rangeGreen.value, rangeBlue.value]);
+ // var color="rgb(" + rangeRed.value + "," + rangeBlue.value + ")"; -- alt satırda ki kod ile aynı
+    var color = `rgb(${rangeRed.value},${rangeGreen.value},${rangeBlue.value})`;
+    var colorRev = `rgb(${255 - rangeRed.value},${255 - rangeGreen.value},${255 - rangeBlue.value})`;
+    pickerDiv.innerHTML=color;
+    pickerDiv.style.backgroundColor=color;
+    pickerDiv.style.color=colorRev;
+}
+
+changeColor();
