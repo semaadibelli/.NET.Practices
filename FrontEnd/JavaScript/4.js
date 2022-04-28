ed07497685a0c4fad6dfa0ac2498a1d4baaad305
semaadibelli <semanur.adibelli@gmail.com>
2022-04-28 12:37:44 +0300

    google map

--- FrontEnd/JavaScript/4.js
+++ FrontEnd/JavaScript/4.js
@@ -1,7 +1,7 @@
 console.log("4.js dosyası çalıştı.");
 
 // Geolocationn
-var getLocation = () => {
+var initMap = () => {
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(showPosition);
     } else {
@@ -9,5 +9,41 @@ var getLocation = () => {
     }
 }
 var showPosition = (position) => {
-    console.log(position);
+ //   console.log(position);
+    var pos ={
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+    };
+    console.log(pos);
+    var mapDiv = document.getElementById('map');
+    var map = new google.maps.Map(mapDiv, {
+        center: pos,
+        zoom: 18,
+        mapTypeId: "terrain",
+    });
+
+    var marker = new google.maps.Marker({
+        position: pos,
+        map: map
+    });
+
+    map.addListener('click', (e) => {
+        console.log(e);
+        console.log(e.latLng.lat())
+        var posClick = {
+            lat: e.latLng.lat(),
+            lng: e.latLng.lng()
+        };
+        //markerları temizle
+        marker.setMap(null);
+        //tıklanan konuma göre marker ekle
+        marker = new google.maps.Marker({
+            position: posClick,
+            map: map,
+            title: 'CLicked Location',
+            animation: google.maps.Animation.DROP
+        });
+    });
+
+
 }
