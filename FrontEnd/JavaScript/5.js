543a51d9afd29fae42e17a3fe71486c195861aba
semaadibelli <semanur.adibelli@gmail.com>
2022-04-29 12:17:58 +0300

    google maps

--- FrontEnd/JavaScript/5.js
+++ FrontEnd/JavaScript/5.js
@@ -1,25 +1,55 @@
 //GeoLocation
+let place = null;
+let places = [];
+let myPosition = null;
+let destinationPosition = null;
+let directionsService = null;
+let directionsRenderer = null;
+let wayPoints = [];
+
+const checkLocalStorage = () => {
+    if (localStorage.getItem("places") === null) {
+        localStorage.setItem("places", JSON.stringify(places));
+    } else {
+        places = JSON.parse(localStorage.getItem("places"));
+        for (let i = 0; i < places.length; i++) {
+            let place = places[i];
+            addPlaceHtml(place);
+        }
+    }
+};
 
 var initMap = () => {
     if (navigator.geolocation) {
+        directionsService = new google.maps.DirectionsService();
+        directionsRenderer = new google.maps.DirectionsRenderer();
         navigator.geolocation.getCurrentPosition(showPosition);
+
     } else {
         console.log("Geolocation is not supported by this browser.");
     }
 }
 
-var showPosition = (position) => {
+const showPosition = (position) => {
     //console.log(position);
-    var pos = {
+    myPosition = {
         lat: position.coords.latitude,
         lng: position.coords.longitude
     };
 
     const map = new google.maps.Map(document.getElementById("map"), {
-        center: pos,
+        center: myPosition,
         zoom: 18,
         mapTypeControl: false,
+        styles: mapStyleArr
     });
+
+    const trafficLayer = new google.maps.TrafficLayer();
+    trafficLayer.setMap(map);
+
+    directionsRenderer.setMap(map); //çiziciyi haritaya bağladık
+    directionsRenderer.setPanel(document.getElementById("mapPanel"));// sonuçlar burada gösterilecek;
+
     const card = document.getElementById("pac-card");
     const input = document.getElementById("pac-input");
     const options = {
@@ -48,7 +78,7 @@ var showPosition = (position) => {
         marker.setVisible(false);
 
         place = autocomplete.getPlace();
-        console.log(place);
+        // console.log(place);
         if (!place.geometry || !place.geometry.location) {
             alert("No details available for input: '" + place.name + "'");
             return;
@@ -61,6 +91,13 @@ var showPosition = (position) => {
             map.setCenter(place.geometry.location);
             map.setZoom(17);
         }
+
+        destinationPosition = {
+            lat: place.geometry.location.lat(),
+            lng: place.geometry.location.lng(),
+        };
+
+        //getDirections(directionsService, directionsRenderer);
 
         marker.setPosition(place.geometry.location);
         marker.setVisible(true);
@@ -83,21 +120,71 @@ const addPlace = () => {
         }
 
         places.push(venue);
-
-        console.log(places);
-        const placeList = document.getElementById("place-list");
-        const placeItem = document.createElement("li");
-        placeList.classList.add("list-group");
-        placeList.classList.add("list-group-flush");
-        placeItem.classList.add("list-group-item");
-        placeItem.innerHTML = `${place.name}`;
-        placeList.appendChild(placeItem);
+        localStorage.setItem("places", JSON.stringify(places));
+        // console.log(places);
+        addPlaceHtml(venue);
         place = null;
     }
 }
 
+const addPlaceHtml = (place) => {
+    const placeList = document.getElementById("place-list");
+    const placeItem = document.createElement("li");
+    placeList.classList.add("list-group");
+    placeList.classList.add("list-group-flush");
+    placeItem.classList.add("list-group-item");
+    placeItem.classList.add("item-silinecek");
+    placeItem.innerHTML = `${place.name}`;
+    placeList.appendChild(placeItem);
+
+    placeItem.addEventListener("click", () => {
+        placeList.removeChild(placeItem);
+        // for (let i = 0; i < places.length; i++) {
+        //     const item = places[i];
+        //     if (item.id == place.id) {
+        //         places.splice(i, 1);
+        //         break;
+        //     }
+        // }
+        places = places.filter(item => item.id !== place.id);
+        localStorage.setItem("places", JSON.stringify(places));
+    });
+}
+
+const getDirections = () => {
+    if (places.length == 0) return;
+
+    wayPoints = [];
+    for (let i = 0; i < places.length; i++) {
+        wayPoints.push({
+            stopover: true,
+            location: { placeId: places[i].id }
+        });
+    }
+    //console.log(wayPoints);
+
+    directionsService.route({
+        origin: myPosition,
+        destination: myPosition,
+        travelMode: "DRIVING",
+        waypoints: wayPoints,
+        optimizeWaypoints: true,
+        drivingOptions: {
+            departureTime: new Date(Date.now()),
+            trafficModel: "bestguess"
+        }
+    }, (response, status) => {
+        if (status === "OK") {
+            //console.log(response);
+            directionsRenderer.setDirections(response);
+        } else {
+            window.alert("Directions request failed due to " + status);
+        }
+    });
+};
+
 const checkPlaces = (venue) => {
-    console.log([places, venue]);
+    //console.log([places, venue]);
     for (let i = 0; i < places.length; i++) {
         const item = places[i];
         if (item.id === venue.id) {
@@ -106,6 +193,141 @@ const checkPlaces = (venue) => {
     }
     return false;
 };
-
-let place = null;
-let places = [];
+//https://snazzymaps.com/style/287720/modest
+const mapStyleArr = [
+    {
+        "featureType": "all",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#202c3e"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "gamma": 0.01
+            },
+            {
+                "lightness": 20
+            },
+            {
+                "weight": "1.39"
+            },
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "weight": "0.96"
+            },
+            {
+                "saturation": "9"
+            },
+            {
+                "visibility": "on"
+            },
+            {
+                "color": "#000000"
+            }
+        ]
+    },
+    {
+        "featureType": "all",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": 30
+            },
+            {
+                "saturation": "9"
+            },
+            {
+                "color": "#29446b"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "saturation": 20
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": 20
+            },
+            {
+                "saturation": -20
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "lightness": 10
+            },
+            {
+                "saturation": -30
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#193a55"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.stroke",
+        "stylers": [
+            {
+                "saturation": 25
+            },
+            {
+                "lightness": 25
+            },
+            {
+                "weight": "0.01"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "lightness": -20
+            }
+        ]
+    }
+];
