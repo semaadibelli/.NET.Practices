a22a64808a8f112dab35fac1d9227e7f05bc6dd7
semaadibelli <semanur.adibelli@gmail.com>
2022-05-05 11:25:43 +0300

    JQuery 1

---
+++ FrontEnd/JavaScript/6.js
@@ -0,0 +1,82 @@
+//DOCUMENT READY
+
+// toggle anahtarlama açığı kapatır kapalıyı açar
+// Aşağıdaki şekilde de document ready yazılabilir.(1.yol)
+// $(() => {
+// 
+// });
+
+//(2.yol)
+$(document).ready(function () { //callback func.
+    $(".form-range").on("input", changeColor); //callback func.
+    changeColor();
+
+    $("#picker-div").on("click", copyClipboard);
+    $("#picker-div").on("mouseover", divMouseOver);
+    $("#picker-div").on("mouseleave", divMouseLeave);
+});
+
+const divMouseOver = () => { 
+    const pickerDiv = $("#picker-div");
+    
+    // pickerDiv.addClass("animate__animated");
+    // pickerDiv.addClass("animate__pulse");
+
+    pickerDiv.toggleClass("animate__animated");
+    pickerDiv.toggleClass("animate__pulse");
+};
+
+const divMouseLeave = () => { 
+    const pickerDiv = $("#picker-div");
+    
+    // pickerDiv.removeClass("animate__animated");
+    // pickerDiv.removeClass("animate__pulse");
+
+    pickerDiv.toggleClass("animate__animated");
+    pickerDiv.toggleClass("animate__pulse");
+};
+//tıklandığı zaman içindeki metni kopyalar.
+const copyClipboard = () => {
+    Swal.fire({
+        icon: 'question',
+        title: 'Kopyalansın mı?',
+        showCancelButton: true,
+        confirmButtonText: 'Kopyala',
+        cancelButtonText: `İptal`,
+    }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        //console.log(result);
+        if (result.isConfirmed) {
+            const pickerDiv = $("#picker-div");
+            //kopyalama işlemi yaptığımız js kodu
+            navigator.clipboard.writeText(pickerDiv.html());
+            Swal.fire('Kopyalandı!', pickerDiv.html(), 'success')
+        } else if (result.isDismissed) {
+            Swal.fire('İptal', 'Kopyalama işlemi iptal edildi', 'info')
+        }
+    })
+};
+
+
+// value okumak için val metodu kullanılır
+
+const changeColor = () => {
+    const rangeRed = $("#range-red");
+    const rangeGreen = $("#range-green");
+    const rangeBlue = $("#range-blue");
+
+    const pickerDiv = $("#picker-div");
+
+   // console.log([rangeRed.val(), rangeGreen.val(),rangeBlue.val()]); // değer okuma
+    const color = `rgb(${rangeRed.val()},${rangeGreen.val()},${rangeBlue.val()})`;
+    const colorRev = `rgb(${255 - rangeRed.val()},${255 - rangeGreen.val()},${255 - rangeBlue.val()})`;
+
+    //1. yol
+    //pickerDiv.html(color);
+    //pickerDiv.css("background-color", color);
+    //pickerDiv.css("color", colorRev);
+
+    //2.yol
+    pickerDiv.html(color).css("background-color", color).css("color", colorRev);
+
+};
