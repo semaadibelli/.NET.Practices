252751ce5866d460ddaf40247e05db26d8c50327
semaadibelli <semanur.adibelli@gmail.com>
2022-05-12 14:40:13 +0300

    repo

---
+++ AdminTemplate/BusinessLogic/Repository/ProductRepo.cs
@@ -0,0 +1,13 @@
+﻿using AdminTemplate.BusinessLogic.Repository.Abstracts.EntityFrameworkCore;
+using AdminTemplate.Data;
+using AdminTemplate.Models.Entities;
+
+namespace AdminTemplate.BusinessLogic.Repository
+{
+    public class ProductRepo : RepositoryBase<Product, Guid>
+    {
+        public ProductRepo(MyContext context) : base(context)
+        {
+        }
+    }
+}
