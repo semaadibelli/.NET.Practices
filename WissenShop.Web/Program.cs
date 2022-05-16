b6130e253406af70601ee5a67b350a188d5f6b3f
Sema Adıbelli <semanur.adibelli@gmail.com>
2022-05-16 21:45:23 +0300

    WissenShop.Web.Core web UI referans

--- WissenShop.Web/Program.cs
+++ WissenShop.Web/Program.cs
@@ -1,14 +1,25 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using WissenShop.Data.EntityFramework;
+using WissenShop.Web.Core.Extensions;
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+var builder = WebApplication.CreateBuilder(args);
+var con1 = builder.Configuration.GetConnectionString("con1");
+builder.Services.AddDbContext<MyContext>(options => options.UseSqlServer(con1));
+
+builder.Services.AddServices();
+
+builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    //app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -18,8 +29,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
+//app.UseSession();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
--- WissenShop.Web2/Program.cs
+++ WissenShop.Web2/Program.cs
@@ -1,4 +1,9 @@
+using WissenShop.Web.Core.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddServices();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
